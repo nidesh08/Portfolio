5fedd3c75ba08386204996cfd07e65cd33bdf63b
nidesh08 <nideshacosta8@gmail.com>
2020-10-16 21:03:03 +0800

    Modifies layout and navigation

--- pages/contactScript.js
+++ pages/contactScript.js
@@ -1,42 +1,54 @@
 
   function modalShow()
   {
-    var name=document.getElementById("name").value;
-    var email=document.getElementById("email").value;
-    var reason=document.getElementById("reason").value;
-    if ((name!="")&&(email!="")&&(reason!=""))
-    {
-        var displayName=document.getElementById("clientName");
-        displayName.innerHTML="Hi "+name+"!";
-        $("#modal").modal({backdrop: "static"});
-    }
-}
-  /*
+      var name=document.getElementById("name").value;
+      var email=document.getElementById("email").value;
+      var reason=document.getElementById("reason").value;
 
-  $('#myForm').on('submit', function(e){
-    $('modall').modal('show');
-    e.preventDefault();
-  });
-  $('#myForm').on('submit', dunct modalshow(e));
-  
-  function submitBtn()
-  {
-    var name=document.getElementById("name").value;
-    var email=document.getElementById("email").value;
-    var reason=document.getElementById("reason").value;
-    if ((name!="")&&(email!="")&&(reason!=""))
-    {
-        var displayName=document.getElementById("clientName");
-        displayName.innerHTML="Hi "+name+"!";
-        
+      
+      if (document.getElementById("name").value == "")
+      {
+        document.getElementById("msg").innerHTML="Please enter your name.";
+      }
+      else
+      {
+        document.getElementById("msg").innerHTML="";
+      }
+
+      if (document.getElementById("email").value == "")
+      {
+        document.getElementById("msg1").innerHTML="Please enter your email.";
+      }
+      else
+      {
+        document.getElementById("msg1").innerHTML="";
+      }
+
+      if  (document.getElementById("reason").value == "")
+      {
+        document.getElementById("msg2").innerHTML="Please select your reason.";
+      }
+      else
+      {
+        document.getElementById("msg2").innerHTML="";
+      }
+
+      if  ((name!="")&&(email!="")&&(reason!=""))
+      {
+          var displayName=document.getElementById("clientName");
+          displayName.innerHTML="Hi "+name+"!";
+          $("#modal").modal({backdrop: "static"});
+        clearr();
     }
-  
   }
-  function modalshow(e)
+
+  function clearr()
   {
-    
-    e.preventDefault();
-    $("#modal").modal({backdrop: "static"});
+      document.getElementById("msg").innerHTML="";
+      document.getElementById("msg1").innerHTML="";
+      document.getElementById("msg2").innerHTML="";
+      document.getElementById("name").value="";
+      document.getElementById("email").value="";
+      document.getElementById("reason").value="";
   }
-*/
-  
+ 
---
+++ pages/menu.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function(){
+    $(".menu").click(function(){
+      $(".buttons").toggleClass("show");
+    });
+  });
